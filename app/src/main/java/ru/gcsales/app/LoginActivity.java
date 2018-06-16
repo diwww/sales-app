523de27314b549460f777630d71cbb34c17a4a8d
Maxim Surovtsev <realitybelieves@gmail.com>
2018-06-16 23:03:19 +0300

    LoginActivity set views and on click listeners.

--- app/src/main/java/ru/gcsales/app/LoginActivity.java
+++ app/src/main/java/ru/gcsales/app/LoginActivity.java
@@ -2,12 +2,53 @@ package ru.gcsales.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private Button mRegisterButton;
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
+        setListeners();
+    }
+
+    private void initViews() {
+        mLoginButton = findViewById(R.id.button_login);
+        mRegisterButton = findViewById(R.id.button_register);
+        mUsernameEditText = findViewById(R.id.edit_text_username);
+        mPasswordEditText = findViewById(R.id.edit_text_password);
+    }
+
+    private void setListeners() {
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsernameEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+
+                Toast.makeText(LoginActivity.this,
+                        String.format("username=%s;password=%s", username, password),
+                        Toast.LENGTH_LONG).show();
+
+                // TODO: other logic
+            }
+        });
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: start RegisterActivity
+            }
+        });
     }
 }
