ed92934a0d14fbef28f984af5f9b6957bfbea9f6
Maxim Surovtsev <realitybelieves@gmail.com>
2018-07-01 12:23:30 +0300

    Shop and product models.

---
+++ app/src/main/java/ru/gcsales/app/Product.java
@@ -0,0 +1,134 @@
+package ru.gcsales.app;
+
+/**
+ * Модель для товара
+ */
+public class Product {
+
+    private int mId;
+    private String mName;
+    private String mCategory;
+    private double mOldPrice;
+    private double mNewPrice;
+    private String mDateIn;
+    private String mDateOut;
+    private String mCrawlDate;
+    private String mCondition;
+    private String mImageUrl;
+    private String mDiscount;
+    private Shop mShop;
+
+    public Product(int id, String name, String category,
+                   double oldPrice, double newPrice, String dateIn,
+                   String dateOut, String crawlDate, String condition,
+                   String imageUrl, String discount, Shop shop) {
+        mId = id;
+        mName = name;
+        mCategory = category;
+        mOldPrice = oldPrice;
+        mNewPrice = newPrice;
+        mDateIn = dateIn;
+        mDateOut = dateOut;
+        mCrawlDate = crawlDate;
+        mCondition = condition;
+        mImageUrl = imageUrl;
+        mDiscount = discount;
+        mShop = shop;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getCategory() {
+        return mCategory;
+    }
+
+    public void setCategory(String category) {
+        mCategory = category;
+    }
+
+    public double getOldPrice() {
+        return mOldPrice;
+    }
+
+    public void setOldPrice(double oldPrice) {
+        mOldPrice = oldPrice;
+    }
+
+    public double getNewPrice() {
+        return mNewPrice;
+    }
+
+    public void setNewPrice(double newPrice) {
+        mNewPrice = newPrice;
+    }
+
+    public String getDateIn() {
+        return mDateIn;
+    }
+
+    public void setDateIn(String dateIn) {
+        mDateIn = dateIn;
+    }
+
+    public String getDateOut() {
+        return mDateOut;
+    }
+
+    public void setDateOut(String dateOut) {
+        mDateOut = dateOut;
+    }
+
+    public String getCrawlDate() {
+        return mCrawlDate;
+    }
+
+    public void setCrawlDate(String crawlDate) {
+        mCrawlDate = crawlDate;
+    }
+
+    public String getCondition() {
+        return mCondition;
+    }
+
+    public void setCondition(String condition) {
+        mCondition = condition;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+    }
+
+    public String getDiscount() {
+        return mDiscount;
+    }
+
+    public void setDiscount(String discount) {
+        mDiscount = discount;
+    }
+
+    public Shop getShop() {
+        return mShop;
+    }
+
+    public void setShop(Shop shop) {
+        mShop = shop;
+    }
+}
---
+++ app/src/main/java/ru/gcsales/app/Shop.java
@@ -0,0 +1,51 @@
+package ru.gcsales.app;
+
+/**
+ * Модель для магазина
+ */
+class Shop {
+
+    private int mId;
+    private String mAlias;
+    private String mName;
+    private String mImageUrl;
+
+    public Shop(int id, String alias, String name, String imageUrl) {
+        mId = id;
+        mAlias = alias;
+        mName = name;
+        mImageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getAlias() {
+        return mAlias;
+    }
+
+    public void setAlias(String alias) {
+        mAlias = alias;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+    }
+}
