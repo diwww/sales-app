ddf5b4d47580593055359bf72fc86b5f7a3e6d24
Maxim Surovtsev <realitybelieves@gmail.com>
2018-07-02 11:21:52 +0300

    Toolbar basic setup.

--- app/src/main/java/ru/gcsales/app/MainActivity.java
+++ app/src/main/java/ru/gcsales/app/MainActivity.java
@@ -2,12 +2,24 @@ package ru.gcsales.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        startActivity(LoginActivity.newIntent(this));
     }
+
+    private void init() {
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
 }
